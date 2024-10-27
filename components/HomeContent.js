420a2f284ffdcd673692b725512803c6637ed1d3
Mathas6509681208 <mathas.sor@dome.tu.ac.th>
2024-10-27 14:25:16 +0700

    แก้ homecontent

--- components/HomeContent.js
+++ components/HomeContent.js
@@ -3,17 +3,49 @@ const HomeContent = () => {
     <div className="bg-white min-h-screen p-6">
       <h1 className="my-24 font-bold text-black text-2xl">RECOMMEND</h1>
       <div className="flex justify-between flex-row gap-20 p-5 my-12">
-        <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
-          Poster
+       
+        <div className="flex flex-col items-center">
+          <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
+            Poster
+          </div>
+          <div className="w-full text-center mt-4 font-semibold">Date</div>
+          <div className="w-full text-center mt-4 font-semibold text-black">Event Title</div>
+          <button className="bg-yellow-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-3xl mt-2">
+            Book Now
+          </button>
         </div>
-        <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
-          Poster
+        
+        <div className="flex flex-col items-center">
+          <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
+            Poster
+          </div>
+          <div className="w-full text-center mt-4 font-semibold">Date</div>
+          <div className="w-full text-center mt-4 font-semibold text-black">Event Title</div>
+          <button className="bg-yellow-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-3xl mt-2">
+            Book Now
+          </button>
         </div>
-        <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
-          Poster
+        
+        <div className="flex flex-col items-center">
+          <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
+            Poster
+          </div>
+          <div className="w-full text-center mt-4 font-semibold">Date</div>
+          <div className="w-full text-center mt-4 font-semibold text-black">Event Title</div>
+          <button className="bg-yellow-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-3xl mt-2">
+            Book Now
+          </button>
         </div>
-        <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
-          Poster
+        
+        <div className="flex flex-col items-center">
+          <div className="w-64 h-80 bg-gray-600 flex items-center justify-center text-white text-xl rounded-lg">
+            Poster
+          </div>
+          <div className="w-full text-center mt-4 font-semibold">Date</div>
+          <div className="w-full text-center mt-4 font-semibold text-black">Event Title</div>
+          <button className="bg-yellow-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-3xl mt-2">
+            Book Now
+          </button>
         </div>
       </div>
     </div>
